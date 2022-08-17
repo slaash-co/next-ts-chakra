9328c4ea34151415cfdbbd46c2a499d14f424e94
nooglersoon <nooglersoon@outlook.co.id>
2022-08-17 08:59:23 +0700

    add custom theme template

---
+++ core/utils/theme.ts
@@ -0,0 +1,22 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+    fonts: {
+        heading: `'Custom Font Name', serif`,
+        body: `'Custom Font Name', serif`,
+    },
+    colors: {
+        primary: {
+            100: "#",
+            500: "#",
+        },
+        secondary: {
+            100: "#",
+        },
+        ternary: {
+            100: "#"
+        }
+    },
+})
+
+export default theme
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import theme from "../core/utils/theme"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <div>
+        <Head>
+          <title>Project Title</title>
+          <meta name="description" content="Project Purpose" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        {/* <Navbar /> */}
+        <Component {...pageProps} />
+        {/* <Footer /> */}
+      </div>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
